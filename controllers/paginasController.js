b60bc500a0e07898292dd8f11997ee779c1ef993
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-01-04 11:55:01 +0100

    feat: :art: Realizando consultas a bbdd
    Realizando consultas a bd para mostrar los viajes disponibles con su info

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -1,3 +1,4 @@
+import {Viaje} from '../models/Viaje.js';
 
 const paginaInicio = (req, res) => { // req -> Lo que enviamos : res -> lo que nos responde
     res.render('inicio', {
@@ -11,9 +12,14 @@ const paginaNosotros = (req, res) => {
     });
 };
 
-const paginaViajes = (req, res) => { 
+const paginaViajes = async (req, res) => { 
+    // Consultar BD
+    const viajes = await Viaje.findAll();
+    console.log(viajes);
+
     res.render('viajes', {
-        pagina: 'Viajes'
+        pagina: 'Próximos Viajes',
+        viajes,
     });
 };
 
