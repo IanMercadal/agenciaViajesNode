b87cbe99ca4e29fc8e12f002e1c4eff02768c7ee
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-01-04 19:35:03 +0100

    feat: :art: Instalando dotenv
    Configurando los archivos .env y de arranque

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import router from './routes/index.js';
 import db from './config/db.js';
+import dotenv from 'dotenv';
+dotenv.config({path:"variables.env"});
 
 const app = express();
 
@@ -8,9 +10,6 @@ const app = express();
 db.authenticate()
     .then( () => console.log("BBDD conectada"))
     .catch( error => console.log("Error"));
-
-// Definir el puerto
-const port = process.env.PORT || 4000;
 
 // Habilitar PUG
 app.set('view engine', 'pug');
@@ -33,7 +32,12 @@ app.use(express.static('./public'));
 // Agregar Router
 app.use('/', router);
 
+/* PUERTO Y HOST PARA LA APP */
+const host = process.env.HOST || '0.0.0.0';
+// Definir el puerto
+const port = process.env.PORT || 4000;
+
 // CALLBACK
-app.listen(port, () => {
+app.listen(port,host, () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
